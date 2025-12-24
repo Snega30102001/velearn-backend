a5dc37762d5d566359fe8dd44fd7d2e6c07fede0
Snega30102001 <snegarameshvimala@gmail.com>
2025-12-24 12:32:40 +0530

    AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -18,12 +18,14 @@ class AuthController extends Controller
                 'name'      => 'required',
                 'email'     => 'required|email|unique:users',
                 'password'  => 'required|min:6',
+                'phone'     => 'required|digits:10',
             ]);
 
             $user = User::create([
                 'name'      => $request->name,
                 'email'     => $request->email,
                 'password'  => Hash::make($request->password),
+                'phone'     => $request->phone,
             ]);
 
             // Default Role
